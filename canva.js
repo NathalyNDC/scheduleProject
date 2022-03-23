ac23841b0d1d9bdc01c00d883ea9c9b5243982e2
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2022-03-23 14:06:18 -0600

    navigationStyle

--- canva.js
+++ canva.js
@@ -1,4 +1,4 @@
-var canvas =  document.getElementById("draw");
+/*var canvas =  document.getElementById("draw");
 var btnClean = document.getElementById("clean");
 var ctx = canvas.getContext("2d");
 resize();
@@ -9,6 +9,7 @@ window.addEventListener("resize", resize);
 document.addEventListener("mousemove", draw);
 document.addEventListener("mousedown", setPosition);
 document.addEventListener("mouseenter", setPosition);
+
 
 btnClean.addEventListener("click",clean);
 
@@ -39,7 +40,7 @@ function draw(e) {
     ctx.beginPath(); // begin the drawing path
     
     // line properties
-    ctx.lineWidth = 10; // width of line
+    ctx.lineWidth = 8; // width of line
     ctx.lineCap = "round"; // rounded end cap
     ctx.strokeStyle = color; // hex color of line
 
@@ -49,4 +50,86 @@ function draw(e) {
      ctx.lineTo(pos.x, pos.y); // to position
   
      ctx.stroke(); // draw it!
-}
+}*/
+
+window.onload = function() {
+	var myCanvas = document.getElementById("draw");
+	var ctx = myCanvas.getContext("2d");
+    
+    // Fill Window Width and Height
+    myCanvas.width = window.innerWidth;
+	myCanvas.height = window.innerHeight;
+	
+	
+    // Mouse Event Handlers
+	if(myCanvas){
+		var isDown = false;
+		var canvasX, canvasY;
+		ctx.lineWidth = 5;
+		
+		$(myCanvas)
+		.mousedown(function(e){
+			isDown = true;
+			ctx.beginPath();
+			canvasX = e.pageX - myCanvas.offsetLeft;
+			canvasY = e.pageY - myCanvas.offsetTop;
+			ctx.moveTo(canvasX, canvasY);
+		})
+		.mousemove(function(e){
+			if(isDown !== false) {
+				canvasX = e.pageX - myCanvas.offsetLeft;
+				canvasY = e.pageY - myCanvas.offsetTop;
+				ctx.lineTo(canvasX, canvasY);
+				ctx.strokeStyle = "#000";
+				ctx.stroke();
+			}
+		})
+		.mouseup(function(e){
+			isDown = false;
+			ctx.closePath();
+		});
+	}
+	
+	// Touch Events Handlers
+	draw = {
+		started: false,
+		start: function(evt) {
+
+			ctx.beginPath();
+			ctx.moveTo(
+				evt.touches[0].pageX,
+				evt.touches[0].pageY
+			);
+
+			this.started = true;
+
+		},
+		move: function(evt) {
+
+			if (this.started) {
+				ctx.lineTo(
+					evt.touches[0].pageX,
+					evt.touches[0].pageY
+				);
+
+				ctx.strokeStyle = "#000";
+				ctx.lineWidth = 8;
+				ctx.stroke();
+			}
+
+		},
+		end: function(evt) {
+			this.started = false;
+		}
+	};
+	
+	// Touch Events
+	myCanvas.addEventListener('touchstart', draw.start, false);
+	myCanvas.addEventListener('touchend', draw.end, false);
+	myCanvas.addEventListener('touchmove', draw.move, false);
+	
+	// Disable Page Move
+	document.body.addEventListener('touchmove',function(evt){
+		evt.preventDefault();
+	},false);
+};
