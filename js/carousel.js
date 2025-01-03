8fd322fadde2c1ad28574ccbf7b9196fc8cef8b1
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2025-01-02 22:48:20 -0600

    modal

--- js/carousel.js
+++ js/carousel.js
@@ -17,3 +17,57 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3600); // Change image every 2 seconds
 }
+
+//MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("music-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+// Get the modal
+var modal2 = document.getElementById("myModal-dressCode");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("btnDressCode");
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
