1d283a7b2d8452d65c10f372c93a4e157fcbf3b4
Nathaly N Dimas C <nathaly.ndc@hotmail.com>
2025-01-04 21:00:36 -0600

    modalesCalendarEvent

--- js/carousel.js
+++ js/carousel.js
@@ -71,3 +71,59 @@ window.onclick = function(event) {
     modal2.style.display = "none";
   }
 }
+
+
+
+// Get the modal
+var modal3 = document.getElementById("myModal-notes");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("btnNotes");
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks the button, open the modal 
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+
+
+
+
+// Get the modal
+var modal4 = document.getElementById("myModal-Confirm");
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("btnConfirm");
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close")[3];
+
+// When the user clicks the button, open the modal 
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
